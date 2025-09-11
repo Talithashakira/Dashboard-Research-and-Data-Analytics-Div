77e5999d5b62b919afc155f5d1d056656be72a8b
Talitha Shakira <tshakira20@gmail.com>
2025-09-11 14:23:20 +0700

    menambahkan visualisasi sentiment dan menambahkan fungsi helper

--- utils/helpers.py
+++ utils/helpers.py
@@ -21,3 +21,23 @@ def get_tags_counts(df, sentiment):
     tag_counts.columns = ["Tag", "Count"]
 
     return tag_counts
+
+def get_tags_sentiment_counts(df):
+    # Ambil kolom Sentiment dan Tags
+    df_tags = df[["Tags_Primary Reason", "Sentiment_Primary Reason"]].dropna()
+
+    # Pecah tags yang dipisahkan "|"
+    df_tags = (
+        df_tags.assign(Tags=df_tags["Tags_Primary Reason"].str.split("|"))
+        .explode("Tags")
+    )
+    df_tags["Tags"] = df_tags["Tags"].str.strip()
+
+    # Hitung jumlah kombinasi Tags x Sentiment
+    tag_counts = (
+        df_tags.groupby(["Tags", "Sentiment_Primary Reason"])
+        .size()
+        .reset_index(name="Count")
+    )
+
+    return tag_counts
